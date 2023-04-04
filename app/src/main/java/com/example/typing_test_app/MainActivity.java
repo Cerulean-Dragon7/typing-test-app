bad3660f5d142fa0784f622f8dd207b0c13e7b77
cerdragon <117027710+ceruleankenneth@users.noreply.github.com>
2023-04-04 17:41:48 +0800

    your friend change 1

--- app/src/main/java/com/example/typing_test_app/MainActivity.java
+++ app/src/main/java/com/example/typing_test_app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.typing_test_app;
 
+import static java.sql.DriverManager.println;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        println("hi this is your friend");
     }
 }

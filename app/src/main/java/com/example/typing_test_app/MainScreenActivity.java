c988ce9e14896658321942de13db5f8fce2e5eca
cerdragon <117027710+ceruleankenneth@users.noreply.github.com>
2023-04-06 18:02:05 +0800

    Profile UI and Test History UI complete ver: 0.0.1

--- app/src/main/java/com/example/typing_test_app/MainScreenActivity.java
+++ app/src/main/java/com/example/typing_test_app/MainScreenActivity.java
@@ -2,13 +2,40 @@ package com.example.typing_test_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreenActivity extends AppCompatActivity {
+    Button testButton, historyButton, boardButton,
+            achievementButton, profileButton, aboutUsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+        testButton = findViewById(R.id.test_button);
+        historyButton = findViewById(R.id.history_button);
+        boardButton = findViewById(R.id.scoreboard_button);
+        achievementButton = findViewById(R.id.achievement_button);
+        profileButton = findViewById(R.id.profile_button);
+        aboutUsButton = findViewById(R.id.credit_button);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainScreenActivity.this,ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainScreenActivity.this,TestHistoryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

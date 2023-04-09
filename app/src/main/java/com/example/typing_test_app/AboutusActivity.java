dadea5e284eea3e90349d09e425d28d09f8b89e9
Mariotham <mariotham1234@gmail.com>
2023-04-10 00:28:22 +0800

    test 7

--- app/src/main/java/com/example/typing_test_app/AboutusActivity.java
+++ app/src/main/java/com/example/typing_test_app/AboutusActivity.java
@@ -26,4 +26,3 @@ public class AboutusActivity extends AppCompatActivity {
         });
     }
 }
-//HI
--- app/src/main/java/com/example/typing_test_app/MainScreenActivity.java
+++ app/src/main/java/com/example/typing_test_app/MainScreenActivity.java
@@ -57,4 +57,3 @@ public class MainScreenActivity extends AppCompatActivity implements View.OnClic
         }
     }
 }
-//HI
